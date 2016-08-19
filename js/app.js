346528967e3b1813d8f507e9ba81274e521b64fe
virgilfowler3 <virgilfowler3@gmail.com>
2016-08-19 17:32:01 -0400

    modified js so that the program could be viewed properly in browser

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 for (var number = 1; number <= 100; number++) {
-	input = "";
+	var input = "";
 	if (number % 3 == 0)
 		input += "Fizz";
 	if (number % 5 == 0)
 		input += "Buzz";
-console.log(input || number);
+var line = input || number; 
+console.log(line + "<br>");
+document.write(line + "<br>");
 }
 
