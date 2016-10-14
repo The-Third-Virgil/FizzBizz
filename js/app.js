0621eab95c7d4122baec2c38698b1341824ad15d
virgilfowler3 <virgilfowler3@gmail.com>
2016-10-14 17:39:34 -0400

    modifications

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 for (var number = 1; number <= 100; number++) {
-	var input = "";
-	if (number % 3 == 0)
-		input += "Fizz";
-	if (number % 5 == 0)
-		input += "Buzz";
-var line = input || number; 
-console.log(line + "<br>");
-document.write(line + "<br>");
+	var output = "";
+		if (number % 3 === 0)
+			output = "Fizz";
+		if (number % 5 === 0)
+			output = "Buzz";
+		if (number % 15 === 0)
+			output = "FizzBuzz";
+	console.log(output || number);
+	var line = output || number;
+	console.log(line + "<br>");
+	document.write(line + "<br>");
 }
-
