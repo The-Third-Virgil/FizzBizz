6a66bb97b2ab18f6534192f7dcc5f40d97f71988
virgilfowler3 <virgilfowler3@gmail.com>
2016-10-20 19:57:11 -0400

    fizzbuzz refractor

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
-for (var number = 1; number <= 100; number++) {
-	var output = "";
-		if (number % 3 === 0)
-			output = "Fizz";
-		if (number % 5 === 0)
-			output = "Buzz";
-		if (number % 15 === 0)
-			output = "FizzBuzz";
-	console.log(output || number);
-	var line = output || number;
-	console.log(line + "<br>");
-	document.write(line + "<br>");
+var myPrompt = parseInt(prompt("What number would you like?"));
+while(isNaN(myPrompt)) {
+  alert("Please enter a whole number");
+  myPrompt = parseInt(prompt("What number would you like?"));
 }
+var fizzBuzz = function(userNumber){
+    for (var number = 1; number <= userNumber; number++) {
+        var output = "";
+        if (number % 3 === 0) {
+        	output = "Fizz";
+        }
+        if (number % 5 === 0) {
+            output = "Buzz";
+        }
+        if (number % 15 === 0) {
+        	output = "FizzBuzz";
+        }   
+        console.log(output || number);
+        var line = output || number;
+        console.log(line + "<br>");
+        document.write(line + "<br>");
+    }
+};
+
+// $(document).ready(function() {
+//   console.log(FizzBuzz());
+// });
+fizzBuzz(myPrompt);
